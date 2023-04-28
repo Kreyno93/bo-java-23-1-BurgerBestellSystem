009ec45f897b025e8416b774041d00b774dac2f0
Kreyno93 <michael.wagener@neuefische.de>
2023-04-28 10:07:36 +0200

    adding .idea to gitignore

--- src/main/java/de/neuefische/Main.java
+++ src/main/java/de/neuefische/Main.java
@@ -1,7 +1,19 @@
 package de.neuefische;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner sc = new Scanner(System.in);
+        OrderSystem orderSystem = new OrderSystem();
+        System.out.println(orderSystem.getMenuMap());
+        System.out.println("Please enter the id of the menu you want to order: ");
+        String id = sc.nextLine();
+        orderSystem.placeOrder(id);
+        try{
+            System.out.println(orderSystem.getOrderById(id));
+        } catch (OrderNotFoundException e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
